81488793c64f167c977d9505f1a129f34265a74c
Antoine monot <antoine@MacBook-Air-de-Antoine.local>
2026-06-10 19:03:25 +0200

    chore: add macOS Xcode project

---
+++ DayPlanner/DayPlannerTests/DayPlannerTests.swift
@@ -0,0 +1,18 @@
+//
+//  DayPlannerTests.swift
+//  DayPlannerTests
+//
+//  Created by Antoine monot on 10/06/2026.
+//
+
+import Testing
+
+struct DayPlannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
